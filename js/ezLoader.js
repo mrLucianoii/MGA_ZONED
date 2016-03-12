a77b98bd8f01419709a20273c2fa9c1bcc509e5a
mrLucianoii <luciano @brandedsolid.com>
2016-03-12 15:17:36 -0800

    Copy Right Stuff

--- js/ezLoader.js
+++ js/ezLoader.js
@@ -1,7 +1,25 @@
 /* #SOLID Designs 
 	Author: Luciano S. Aldana II
 	url: http://brandedsolid.com
-*/
+
+/* SOLID CONFIDENTIAL
+ * __________________
+ * 
+ *  [2013] - [2016] Property of Luciano Aldana dba SOLID 
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of SOLID and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to SOLID
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SOLID.
+ */
+
+
 
 jQuery(document).ready(function () {
 
